c5f14fefc0781336ad9ec94ab7d4c9912486f10c
Aidhan <me@aidhan.net>
2023-05-06 07:31:10 +0000

    Take it back to bare bones

--- app/routes/streaming.tsx
+++ app/routes/streaming.tsx
@@ -1,11 +1,12 @@
-import { type LoaderArgs, json } from "@vercel/remix";
-import { useLoaderData } from "@remix-run/react";
+import { type LoaderArgs, defer } from "@vercel/remix";
+import { Await, useLoaderData } from "@remix-run/react";
 import { parseVercelId } from "~/lib/parse-vercel";
+import { Suspense } from "react";
 
 export const config = { runtime: "edge" };
 export async function loader({ request }: LoaderArgs) {
   const parsedId = parseVercelId(request.headers.get("x-vercel-id"));
-  return json({
+  return defer({
     success: true,
     error: false,
     message: "Hello from the edge!",
@@ -22,11 +23,14 @@ export default function App() {
   const { success, error, message, data } = useLoaderData<typeof loader>();
   return (
     <main>
-      <h1>{message}</h1>
-      <p>Success: {success.toString()}</p>
-      <p>Error: {error.toString()}</p>
-      <p>Edge Region: {data.edgeRegion}</p>
-      <p>Compute Region: {data.computeRegion}</p>
+      <Suspense fallback={<strong>Loading...</strong>}>
+        <h1>{message}</h1>
+        <p>Success: {success.toString()}</p>
+        <p>Error: {error.toString()}</p>
+
+        <p>Edge Region: {data.edgeRegion}</p>
+        <p>Compute Region: {data.computeRegion}</p>
+      </Suspense>
     </main>
   );
 }
