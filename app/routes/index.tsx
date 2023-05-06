0f0ac886a9a6f7f33c2c57794f7ce56119931243
Aidhan <me@aidhan.net>
2023-05-06 08:02:30 +0000

    Fix the streaming response

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,7 @@
 import { Link } from "@remix-run/react";
 
+export const config = { runtime: "edge" };
+
 export default function App() {
   return (
     <div>
--- app/routes/streaming.tsx
+++ app/routes/streaming.tsx
@@ -1,35 +1,51 @@
-import { type LoaderArgs, defer } from "@vercel/remix";
+import { type LoaderArgs, defer, json } from "@vercel/remix";
 import { Await, useLoaderData } from "@remix-run/react";
 import { parseVercelId } from "~/lib/parse-vercel";
 import { Suspense } from "react";
 
 export const config = { runtime: "edge" };
+
+function sleep(val: any, ms: number) {
+  return new Promise((resolve) => setTimeout(() => resolve(val), ms));
+}
+
 export async function loader({ request }: LoaderArgs) {
   const parsedId = parseVercelId(request.headers.get("x-vercel-id"));
   return defer({
-    success: true,
     error: false,
     message: "Hello from the edge!",
-    data: {
-      edgeRegion: parsedId.proxyRegion,
-      computeRegion: parsedId.computeRegion,
-    },
+
+    edgeRegion: sleep(parsedId.proxyRegion, 250),
+    computeRegion: sleep(parsedId.computeRegion, 500),
   });
 }
 
 export function headers({ request }: LoaderArgs) {}
 
 export default function App() {
-  const { success, error, message, data } = useLoaderData<typeof loader>();
+  const { error, message, edgeRegion, computeRegion } =
+    useLoaderData<typeof loader>();
   return (
     <main>
-      <Suspense fallback={<strong>Loading...</strong>}>
-        <h1>{message}</h1>
-        <p>Success: {success.toString()}</p>
-        <p>Error: {error.toString()}</p>
-
-        <p>Edge Region: {data.edgeRegion}</p>
-        <p>Compute Region: {data.computeRegion}</p>
+      <Suspense fallback={"loading..."}>
+        <Await resolve={message}>{(message) => <h1>{message}</h1>}</Await>
+        <br />
+        <p>
+          Edge Region:{" "}
+          <Suspense fallback={"loading..."}>
+            <Await resolve={edgeRegion}>{(region) => region}</Await>
+          </Suspense>
+        </p>
+        <p>
+          Compute Region:{" "}
+          <Suspense fallback={"loading..."}>
+            <Await resolve={computeRegion}>{(region) => region}</Await>
+          </Suspense>
+        </p>
+        <Await resolve={error}>
+          {error && <h1>Error</h1>}
+          {error && <p>{message}</p>}
+        </Await>
       </Suspense>
     </main>
   );
